513a1ee6ccbf07ee36d20a1de0098f861f912f49
AlbertContra <daturafrox@outlook.com>
2020-12-17 14:55:17 -0600

    Ready to build the image using the dockerfile

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 module.exports = {
     
     PORT: 3010,
-    DB_URI: 'mongodb://localhost:27017',
+    DB_URI: 'mongodb://host.docker.internal:27017',
     DB_DATABASE: 'pprueba_docker',
     DB_USER: '',
     DB_PASSWORD:'',
